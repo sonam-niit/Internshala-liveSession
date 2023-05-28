2b3635599fa18db0e9f5f725f8cf1ab817a1f0af
sonam-niit <sonamlovesmumypapa@gmail.com>
2023-05-28 15:34:27 +0530

    mongodb

--- mongodb/connect.js
+++ mongodb/connect.js
@@ -1,8 +1,28 @@
-const {MongoClient}= require('mongodb');
+const { MongoClient } = require('mongodb');
 
-const url="mongodb://127.0.0.1:27017/"
-const client= new MongoClient(url);
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
 
-client.connect()
-.then(()=>{console.log("Connected..!!!")})
-.catch((err)=>{console.log('Failed to connect: ',err)})
+async function connect() {
+    try {
+        const conn = await client.connect();
+        console.log("connected");
+    } catch (error) {
+        console.log("Error Occured ", error)
+    }
+}
+async function createCollection() {
+    try {
+        await client.connect();
+        const db=client.db("mydb");
+        const collection= db.collection('employee')
+        console.log("collection created");
+    } catch (error) {
+        console.log("Error  while creating collection ", error)
+    }
+}
+createCollection();
+
+// client.connect()
+// .then(()=>{console.log("Connected Successfully....!!!")})
+// .catch(()=>{console.log("Failed to connect")})
